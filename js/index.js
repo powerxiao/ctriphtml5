4c2be6b43d38943de3a86c4f8535ceb99b56a6c7
wansan <494981499@qq.com>
2016-02-24 10:48:24 +0800

    的

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * Created by wansan on 2016/2/24.
  */
 (function (win) {
-    $(".navList a").tap(function () {
+    $(".navList a").on("click",function () {
         console.log($(this).text());
         $(this).addClass("click-scale").on("webkitTransitionEnd", function () {
             $(this).removeClass("click-scale").css({"-webkit-transition":"all 0.2s ease-out"});
